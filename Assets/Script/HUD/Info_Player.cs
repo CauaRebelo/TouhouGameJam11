cda97d467aa94e5841c6e41c8ccf7b7229d8dcb4
BebeYoshi <cauarebelo@gmail.com>
2023-09-01 16:27:21 -0400

    Variaveis globais

---
+++ Assets/Script/HUD/Info_Player.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Info_Player : MonoBehaviour
+{
+    private static int p_extra = 0;
+    private static int p_deaths = 0;
+    private static int p_jumps = 0;
+    private static int p_death_fall = 0;
+    private static int p_death_enemy1 = 0;
+    private static int p_death_enemy2 = 0;
+    private static int p_death_projectile1 = 0;
+    private static int p_death_projectile2 = 0;
+
+    public static int extra
+    {
+        get { return p_extra; }
+        set { p_extra = value; }
+    }
+
+    public static int deaths
+    {
+        get { return p_deaths; }
+        set { p_deaths = value; }
+    }
+
+    public static int jumps
+    {
+        get { return p_jumps; }
+        set { p_jumps = value; }
+    }
+
+    public static int death_fall
+    {
+        get { return p_death_fall; }
+        set { p_death_fall = value; }
+    }
+
+    public static int death_enemy1
+    {
+        get { return p_death_enemy1; }
+        set { p_death_enemy1 = value; }
+    }
+
+    public static int death_enemy2
+    {
+        get { return p_death_enemy2; }
+        set { p_death_enemy2 = value; }
+    }
+
+    public static int death_projectile1
+    {
+        get { return p_death_projectile1; }
+        set { p_death_projectile1 = value; }
+    }
+
+    public static int death_projectile2
+    {
+        get { return p_death_projectile2; }
+        set { p_death_projectile2 = value; }
+    }
+}
