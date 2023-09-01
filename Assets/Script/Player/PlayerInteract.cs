ef596bc18e54f16aaef08bda2e60efa9f2a83991
Matheus Mendes <mendes.souza.mat@gmail.com>
2023-09-01 20:23:14 -0300

    InputAsset uptade

---
+++ Assets/Script/Player/PlayerInteract.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteract : MonoBehaviour
+{
+    void Update()
+    {
+        
+    }
+    
+    private void OnInteractAction(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+
+        }
+    }
+
+    private void DisplayUI()
+    {
+
+    }
+}
