1cd3d321ad5111f34494a2f0facedd5e55c77911
BebeYoshi <cauarebelo@gmail.com>
2023-09-01 20:26:18 -0400

    Bnuy se mexer

--- Assets/Script/Enemy/EnemyBunnyBehaviour.cs
+++ Assets/Script/Enemy/EnemyBunnyBehaviour.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class EnemyBunnyBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public List<Transform> waypoints;
+
+    public int nextPoints = 0;
+    int pointChangeValue = 1;
+    private float movementSpeed = 2;
+
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveToNextPoint();
+    }
+
+    void MoveToNextPoint()
+    {
+        Transform goalPoint = waypoints[nextPoints];
+        if (goalPoint.transform.position.x > transform.position.x)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else
+            transform.localScale = new Vector3(1, 1, 1);
+        transform.position = Vector2.MoveTowards(transform.position, goalPoint.position, movementSpeed*Time.deltaTime);
+        if(Vector2.Distance(transform.position, goalPoint.position)<1f)
+        {
+            if(nextPoints == waypoints.Count - 1)
+               pointChangeValue = -1;
+            if(nextPoints == 0)
+                pointChangeValue = 1;
+            nextPoints += pointChangeValue;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.name == "Player")
+        {
+            col.gameObject.GetComponent<Death>.Reincarnate();
+        }
     }
 }
