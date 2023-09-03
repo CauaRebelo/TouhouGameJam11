47600a947f8630f94214cae1dd4ca397aa05b2fc
Matheus Mendes <mendes.souza.mat@gmail.com>
2023-09-03 17:11:03 -0300

    Player Animations

--- Assets/Script/Player/AnimationController.cs
+++ Assets/Script/Player/AnimationController.cs
@@ -3,31 +3,72 @@ using UnityEngine;
 public class AnimationController : MonoBehaviour
 {
     [SerializeField] private Animator agentAnimator;
+    [SerializeField] private PlayerItemGrab playerItemGrab;
 
     private void SetWalkAnimation(bool val)
     {
         agentAnimator.SetBool("Walk", val);
     }
 
+    private void SetItemWalkAnimation(bool val)
+    {
+        agentAnimator.SetBool("ItemWalk", val);
+    }
+
     private void SetJumpAnimation(bool val)
     {
         agentAnimator.SetBool("Grounded", val);
+
+        if (!val && playerItemGrab.itemCarry)
+        {
+            agentAnimator.SetBool("ItemJump", true);
+        }
+        else
+        {
+            agentAnimator.SetBool("ItemJump", false);
+        }
     }
 
     public void AnimatePlayerWalk(float velocity)
     {
-        if (velocity > 0)
+        // Se player não carrega um item
+        if (!playerItemGrab.itemCarry)
         {
-            SetWalkAnimation(true);
+            SetItemWalkAnimation(false);
+
+            if (velocity > 0)
+            {
+                SetWalkAnimation(true);
+            }
+            else if (velocity < 0)
+            {
+                SetWalkAnimation(true);
+            }
+            else
+            {
+                SetWalkAnimation(false);
+            }
         }
-        else if (velocity < 0)
-        {
-            SetWalkAnimation(true);
-        }
+
+        // Se player carrega um item
         else
         {
-            SetWalkAnimation(false);
+            SetItemWalkAnimation(true);
+
+            if (velocity > 0)
+            {
+                SetWalkAnimation(true);
+            }
+            else if (velocity < 0)
+            {
+                SetWalkAnimation(true);
+            }
+            else
+            {
+                SetWalkAnimation(false);
+            }
         }
+        
     }
 
     public void AnimatePlayerJump(bool grounded)
